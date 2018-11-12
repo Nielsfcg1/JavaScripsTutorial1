7971a0dc51320634ff66bd3f925ef52f3b555b62
Niels Tromp <nielsfcg1@gmail.com>
2018-11-12 10:48:15 +0100

    college 79

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -8,16 +8,38 @@ var budgetController = (function()
 // UI CONTROLLER
 var UIController = (function(){
 
-//some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+return{
+    getInput: function() {
+
+        return {
+            type: document.querySelector(DOMstrings.inputType).value, // Can be income or expense
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+        }
+       
+    },
+    getDOMStrings: function() {
+        return DOMstrings;
+    }
+
+};
 
 })();
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UIController.getDOMStrings();
     var ctrlAddItem = function() {
         
         // 1. Get the field input data
-
+        var input = UICtrl.getInput();
+        console.log(input);
         // 2. add the item to the budget controller
 
         // 3. add the item to the UI
@@ -27,7 +49,7 @@ var controller = (function(budgetCtrl, UICtrl){
         // 5. display the budget on the UI
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
