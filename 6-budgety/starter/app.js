5abe62eac0af17cb7e08eb1ed0819df43627930a
Niels Tromp <nielsfcg1@gmail.com>
2018-11-14 22:48:37 +0100

    test commit

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -89,6 +89,7 @@ var UIController = (function()
                 element = DOMstrings.expensesContainer;
                      html =  '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div> <div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
+            
 
             // replace placeholder text with actual data
             newHTML = html.replace('%id%', obj.id);
